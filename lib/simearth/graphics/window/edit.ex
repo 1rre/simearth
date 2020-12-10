b5e43ded7283c97ddda256b6452964310d9b745a
Timothy Moores <info@tmoor.es>
2020-12-10 00:29:09 +0000

    added control panel to edit window

--- lib/simearth/graphics/window/edit.ex
+++ lib/simearth/graphics/window/edit.ex
@@ -7,19 +7,25 @@ defmodule Simearth.Graphics.Window.Edit do
 
   def init([parent]) do
     window = :wxWindow.new(parent, 10)
-
+    
     frame = :wxFrame.new(window, 10, @title, size: @size)
     :wxFrame.connect(frame, :size)
     :wxFrame.connect(frame, :close_window)
-    panel = :wxPanel.new(frame, [])
-
+    panel = :wxPanel.new(frame, pos: {200,0}, size: {400,600})
+    ctrl_panel = :wxPanel.new(frame, pos: {0,0}, size: {200,600})
+    ctrl_box = :wxStaticBox.new(ctrl_panel, 0, "Edit Controls", pos: {0,0}, size: {200,600})
     :wxFrame.show(frame)
-    state = %{panel: panel}
+    state = %{panel: panel, ctrl_panel: ctrl_panel, controls: ctrl_box}
     {frame, state}
   end
 
-  def handle_event({:wx, _, _, _, {:wxSize, :size, size, _}}, state = %{panel: panel}) do
-    :wxPanel.setSize(panel, size)
+  def handle_event({:wx, _, _, _, {:wxSize, :size, {x,y}, _}}, state = %{panel: panel, ctrl_panel: ctrl_panel, controls: controls}) do
+    # How to scale the panels?
+    # This won't work if the size is <200
+    # Currently I'm split between setting a min size & setting the size to the min of width & 200 or something
+    :wxStaticBox.setSize(controls, {200, y})
+    :wxPanel.setSize(panel, {x - 200, y})
+    :wxPanel.setSize(ctrl_panel, {200, y})
     {:noreply, state}
   end
 
